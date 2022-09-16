75e238c7b32899edc07664899754dd28efb5ff4f
Tim Wheeler <tim.wheeler@ocius.technology>
2022-09-16 14:00:23 +0800

    Reworking bundling process

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const DtsBundleWebpack = require('dts-bundle-webpack');
-const { IgnorePlugin } = require('webpack');
+var webpack = require('webpack');
+
+
+var version = JSON.stringify(require("./package.json").version).replace(/\"/g, '');
+var bannerOptions = {
+  banner: 'lightjx v' + version + ' - https://github.com/TjWheeler/lightjx',
+  entryOnly: true
+};
+
 module.exports = {
   mode: "production",
   entry: path.resolve(__dirname, './src/index.ts'),
@@ -13,6 +20,7 @@ module.exports = {
         use: 'ts-loader',
         exclude: [/node_modules/],
       },
+      { test: /\.txt$/, use: 'raw-loader' }
     ],
   },
   resolve: {
@@ -21,6 +29,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    clean: true,
     library: {
       name: 'lightjx',
       type: 'umd',
@@ -28,15 +37,16 @@ module.exports = {
   },
 
   plugins: [
-    new IgnorePlugin({resourceRegExp: /^\.\/locale$/,
-    contextRegExp: /moment$/,}),
+    new webpack.IgnorePlugin({ resourceRegExp: /^\.\/locale$/, contextRegExp: /moment$/, }),
     new DtsBundleWebpack({
       name: 'lightjx',
       main: './build/**/*.d.ts',
       out: '../dist/index.d.ts',
       removeSource: true,
-      outputAsModuleFolder: true
+      outputAsModuleFolder: true,
+      headerPath: "header.txt"
     }),
-    new CleanWebpackPlugin(),
+    new webpack.BannerPlugin(bannerOptions),
+    
   ]
 };
